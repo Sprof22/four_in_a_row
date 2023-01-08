7b2f2098caaeaa72287e6033fd88416872cc81c9
Sprof22 <richmondelaigwu@gmail.com>
2023-01-08 07:43:18 +0100

    minor commit

--- Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Board.js
+++ Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Board.js
@@ -1,7 +1,21 @@
 class Board {
     constructor(){
-        this.rows = row;
-        this.columns = columns;
-        this.spaces = [];
+        this.rows = 6;
+        this.columns = 7;
+        this.spaces = this.createSpaces();
+    }
+
+    createSpaces(){
+        const spaces = [];
+        for(let x = 0; x<this.rows; x++){
+            const column = [];
+            for (let y = 0; y< this.columns; y++){
+                const space = new Space (x, y)
+            }
+
+            spaces.push(column);
+        }
+
+        return spaces;
     }
 }
