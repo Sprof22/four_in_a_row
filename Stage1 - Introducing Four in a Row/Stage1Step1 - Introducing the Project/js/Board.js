902055304d3109edd5c8d285bc66db5fb58b5aed
Sprof22 <richmondelaigwu@gmail.com>
2023-01-09 01:16:43 +0100

    leggo

--- Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Board.js
+++ Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Board.js
@@ -7,15 +7,21 @@ class Board {
 
     createSpaces(){
         const spaces = [];
-        for(let x = 0; x<this.rows; x++){
+        for (let x = 0; x < this.columns; x++){
             const column = [];
-            for (let y = 0; y< this.columns; y++){
-                const space = new Space (x, y)
+            for (let y = 0; y< this.rows; y++){
+                const space = new Space (x, y);
+                column.push(space);
             }
-
             spaces.push(column);
         }
+    };
 
-        return spaces;
+    drawHTMLBoard(){
+        for (let column of this.spaces) {
+            for (let space of column){
+                space.drawSVGSpace();
+            }
+        }
     }
 }
