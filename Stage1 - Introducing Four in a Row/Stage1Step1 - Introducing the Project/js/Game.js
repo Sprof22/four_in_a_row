e6c434df72d6e099c0025b74646cc349216b105f
Sprof22 <richmondelaigwu@gmail.com>
2023-01-09 01:16:37 +0100

    draw svg

--- Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Game.js
+++ Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Game.js
@@ -1,3 +1,25 @@
 class Game {
+    constructor(){
+        this.board = new Board;
+        this.player = this.createPlayers();
+        this.ready = false;
+    }
+    /** 
+ * Creates two player objects
+ * @return  {Array}    An array of two Player objects.
+ */
+    createPlayers(){
+        const players = [new Player ('Player 1', 1, '#e15258', false), 
+                    new Player('Player 2', 2, '#e15258')];
+        return players;
+    }
+    /**
+     * Gets game ready
+     */
+    startGame(){
     
+    }
 }
+
+
+
--- Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Token.js
+++ Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Token.js
@@ -5,4 +5,13 @@ class Token {
         this.dropped= false;
     }
 
+    drawHTMLToken(){
+        const Token = document.createElement('div');
+        document.getElementById('game-board-underlay').appendChild(token);
+        token.setAttribute('id', this.id);
+        token.setAttribute('class', 'token');
+        token.style.background.color = this.owner.color;
+    }
+
 }
+
