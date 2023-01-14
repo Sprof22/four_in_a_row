a4fd9832c12e36730154cc47c1bf673b5313fa99
Sprof22 <richmondelaigwu@gmail.com>
2023-01-14 22:18:16 +0100

    this good

--- js/Game.js
+++ js/Game.js
@@ -80,7 +80,7 @@ class Game {
 
 
     /** 
-     * Updates game state after token is dropped. k
+     * Updates game state after token is dropped.
      * @param   {Object}    token - The token that's being dropped.
      * @param   {Object}    target - Targeted space for dropped token.
      */
